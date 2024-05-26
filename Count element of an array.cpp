1b9ca8114aba9a025306812542d69a3ea9d4912b
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-05-26 16:12:52 +0530

    Add files via upload

---
+++ Count element of an array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+
+    int hash[13]={0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]]+=1;
+    }
+    
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<hash[num]<<endl;
+    }
+
+
+}
