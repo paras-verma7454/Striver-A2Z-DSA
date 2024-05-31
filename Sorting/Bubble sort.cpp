9b8b262b3d424d77ea3d65057098228fdb65a669
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-05-31 22:22:56 +0530

    Update Bubble sort.cpp

--- Sorting/Bubble sort.cpp
+++ Sorting/Bubble sort.cpp
@@ -1,28 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-void Bubble_Sort(int arr[],int n){
-    for(int i=n-1;i>=0;i--){
-        for(int j=0;j<=i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=temp;
+
+void Bubble_Sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int swap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                swap = 1;
             }
         }
-        
+        if (swap == 0)
+        {
+            break;
+        }
     }
 }
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    Bubble_Sort(arr,n);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    Bubble_Sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-
 }
