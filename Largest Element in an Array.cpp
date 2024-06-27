ef7dafb34ab8c63df40eac0607fa273bbceb682d
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-06-27 14:57:49 +0530

    Add files via upload

---
+++ Largest Element in an Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Largest(int arr[],int n){
+    int largest=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+    }
+    cout<<largest;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Largest(arr,n);
+}
