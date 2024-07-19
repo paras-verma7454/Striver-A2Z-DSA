e05fcdb7e3ca5a2b184ec77f55440eee9b1714cb
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-07-20 00:32:17 +0530

    Add files via upload

---
+++ Array/Medium/Sort Colors.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 75. Sort Colors
+// Sort an array of 0's 1's and 2's
+
+void SortColors(vector<int> &nums){
+    int low = 0, mid = 0, high = nums.size()-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    SortColors(nums);
+    for(int i=0;i<n;i++){
+        cout<<nums[i]<<" ";
+    }
+}
