4b240131275409cafcb6b512a66a1de73bcb5bf5
Paras Verma <134628559+paras-verma7454@users.noreply.github.com>
2024-07-21 16:29:02 +0530

    Update Majority Element.cpp

--- Array/Medium/Majority Element.cpp
+++ Array/Medium/Majority Element.cpp
@@ -4,9 +4,27 @@ using namespace std;
 // 169. Majority Element
 
 int MajElem(vector<int> &nums){
-    int n=nums.size();
-    sort(nums.begin(),nums.end());
-    return nums[n/2];
+    int n = nums.size();
+    int cnt = 0;
+    int ele;
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            ele = nums[i];
+        } else if (nums[i] == ele) {
+            cnt++;
+        } else
+            cnt--;
+    }
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == ele)
+            cnt1++;
+        if (cnt1 > n / 2)
+            return ele;
+    }
+    return -1;
+    
     
 }
 
